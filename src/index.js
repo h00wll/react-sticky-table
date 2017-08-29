38f27dbabc73442bbe2aca07b7d8b0100fc9d27f
Audrius <h00wll@gmail.com>
2017-08-29 16:46:15 +0300

    Use PropTypes from prop-types package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 var _ = require('underscore');
 var proxy = require('nodeproxy');
